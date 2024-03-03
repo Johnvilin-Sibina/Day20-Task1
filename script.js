d28170fa534c873d617d2c0f22f9898ece266205
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-03-03 16:44:42 +0530

    Command lines are added

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Post Office
+//Pincode is given bythe user and using the pincode information about the post office is displayed
+
+
+//This function is to create h1 tag and this function takes tagname, attrname, attrvalue and content as its parameters
 function create_h1(tagname, attrname, attrvalue, content) {
   var res = document.createElement(tagname);
   res.setAttribute(attrname, attrvalue);
@@ -6,11 +9,14 @@ function create_h1(tagname, attrname, attrvalue, content) {
   return res;
 }
 
+//This function is to create section tag and this function takes not parameter
 function create_section() {
   var res = document.createElement("section");
   return res;
 }
 
+//This function is to create label tag and this function takes tagname, attrname, attrvalue, attr2name,
+//attr2value and content as its parameters
 function create_label(tagname, attrname, attrvalue, attr2name, attr2value, content) {
   var res = document.createElement(tagname);
   res.setAttribute(attrname, attrvalue);
@@ -19,6 +25,8 @@ function create_label(tagname, attrname, attrvalue, attr2name, attr2value, conte
   return res;
 }
 
+//This function is to create input field and this function takes tagname, attr1name, attr1value,
+//attr2name and attr2value as its parameters
 function create_input(tagname, attr1name, attr1value, attr2name, attr2value) {
   var res = document.createElement(tagname);
   res.setAttribute(attr1name, attr1value);
@@ -26,10 +34,14 @@ function create_input(tagname, attr1name, attr1value, attr2name, attr2value) {
   return res;
 }
 
+//This function is to create break and this function takes tagname as its parameter
 function create_break(tagname) {
   var res = document.createElement(tagname);
   return res;
 }
+
+//This functionis to create button and this function takes tagname, attr1name, attr1value, attr2name, attr2value,
+//attr3name, attr3value and content as its parameters
 function create_button(tagname, attr1name, attr1value, attr2name, attr2value, attr3name, attr3value, content) {
   var res = document.createElement(tagname);
   res.setAttribute(attr1name, attr1value);
@@ -39,37 +51,49 @@ function create_button(tagname, attr1name, attr1value, attr2name, attr2value, at
   return res;
 }
 
+//This function is to create div and this function takes tagname,attrname and attrvalue as its parameters
 function create_div(tagname,attrname,attrvalue){
   var res = document.createElement(tagname);
   res.setAttribute(attrname,attrvalue);
   return res;
 }
 
+//This function is to reset the input field after submission
 function reset() {
   var reset = document.getElementById("search").value = ""
   return reset;
 }
 
+//This function is called when the button is clicked and this function calls two functions data() and reset()
 function foo() {
   data();
   reset();
 }
 
+//The function create_h1 is called here twice and two h1 tags are created and values for the parameters are passed
 var heading1 = create_h1("h1", "class", "text", "Know the Pincode but Don't Know the Post Office!")
 var heading2 = create_h1("h1", "class", "text", "My Website is Here to Help You")
 
+//The function create_section is called here and a section tag is created and values for the parameters are passed
 var section = create_section();
 
+//The function create_label is called here and a label tag is created here and values for the parameters are passed
 var label = create_label("label", "for", "search", "class", "main", "Enter the Pincode here:")
 
+//The function create_break is called here and a break tag is created here
 var br1 = create_break("br");
 
-
+//THe function create_input is called here and a input field is created and values for the parameters are passed 
 var input = create_input("input", "type", "text", "id", "search", "class", "field")
 
+//The function create_break is called here twice and two break tags are created here
 var br2 = create_break("br");
-
 var br3 = create_break("br");
+
+//The function create_button is called here and a button is created and values for the parameters are passed
+//when the button is clicked the function foo is called - the function foo calls two functions - data() and reset()
+//The function data fetches the data from the API and displays it and the function reset clears the input field once the 
+//submit button is clicked
 
 var button = create_button("button", "type", "button", "class", "submit", "onclick", "foo()", "Submit")
 
@@ -83,6 +107,8 @@ var container = create_div("div","class","container");
 var row = create_div("div","class","row");
 row.classname = "row";
 
+//This function gets the pincode from the input field that is given by the user and uses the pincode provided by the user
+//and  fetched data about the Post Offices with that pincode and displays the fetched data
 async function data() {
   try{
     var pin = document.querySelector("input").value;
